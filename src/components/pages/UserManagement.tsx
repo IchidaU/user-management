e7f924e3677c33b7d018f0d650f0b37122a97ca5
IchidaU <koo.be.koo@gmail.com>
2025-03-18 09:28:42 +0900

    add: ユーザー詳細モーダル追加

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { FC, memo, useEffect } from "react";
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useCallback, useEffect, useState } from "react";
+import {
+  Center,
+  Dialog,
+  Spinner,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "@/hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  const [open, setOpen] = useState(false);
   const { getUsers, users, loading } = useAllUsers();
+  // const { onSelectUser, selectUser } = useSelectUser();
 
   useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => {
+    // onSelectUser({ id, users });
+    setOpen(true);
+  }, []);
 
   return (
     <>
@@ -24,11 +38,24 @@ export const UserManagement: FC = memo(() => {
                 imageUrl="https://picsum.photos/300"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Dialog.Root
+        // user={selectUser}
+        open={open}
+        onOpenChange={(e) => setOpen(e.open)}
+      >
+        <Dialog.Backdrop />
+        <Dialog.Content>
+          <Dialog.Body>
+            <Text>test</Text>
+          </Dialog.Body>
+        </Dialog.Content>
+      </Dialog.Root>
     </>
   );
 });
